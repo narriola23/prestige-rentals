f55131dc9010074e916d6559eb70396d04585bae
narriola23 <narriola23@gmail.com>
2026-06-20 23:08:18 -0500

    Fix build: add pg to serverComponentsExternalPackages

--- next.config.js
+++ next.config.js
@@ -1,9 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    domains: ['picsum.photos'],
-    unoptimized: true,
-  },
+    images: {
+          domains: ['picsum.photos'],
+          unoptimized: true,
+    },
+    experimental: {
+          serverComponentsExternalPackages: ['pg'],
+    },
 };
 
 module.exports = nextConfig;
