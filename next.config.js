97e679b294d1aeaafbd6cfe8a91e23c7d62b1ab1
narriola23 <narriola23@gmail.com>
2026-06-20 23:18:32 -0500

    Fix build: add explicit webpack @ path alias

--- next.config.js
+++ next.config.js
@@ -1,12 +1,18 @@
+const path = require('path');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-          domains: ['picsum.photos'],
-          unoptimized: true,
-    },
-    experimental: {
-          serverComponentsExternalPackages: ['pg'],
-    },
+      images: {
+              domains: ['picsum.photos'],
+              unoptimized: true,
+      },
+      experimental: {
+              serverComponentsExternalPackages: ['pg'],
+      },
+      webpack: (config) => {
+              config.resolve.alias['@'] = path.resolve(__dirname);
+              return config;
+      },
 };
 
 module.exports = nextConfig;
